b7ae64640b0f91536f72785e6b193c5c6cd2da7d
Xiaocheng Sun <apple.splandy@gmail.com>
2022-09-05 17:16:01 -0400

    complete lab0

---
+++ lab0/welcome.cpp
@@ -0,0 +1,19 @@
+/* 
+ * Name: Xiaocheng Sun
+ * Date: 9/5/2022
+ * Lab 0
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Welcome to CS 15!" << endl;
+  cout<< "Xiaocheng Sun's first program."<<endl;
+
+  // Add your new message here.
+
+  return 0;
+}
